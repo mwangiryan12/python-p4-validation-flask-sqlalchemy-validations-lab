9a02045afb2c2e34be656f2daa07f91d48332562
mwangiryan12 <162281245+mwangiryan12@users.noreply.github.com>
2024-07-18 15:52:26 +0300

    Update models.py

--- server/models.py
+++ server/models.py
@@ -12,6 +12,26 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates("name")
+    def validate_name(self,key,value):
+        if not value:
+            raise ValueError("Author must have a name")
+        elif Author.query.filter_by(name = value).first():
+            raise ValueError("Author name taken")
+        return value
+    
+    @validates("phone_number")
+    def validate_number(self,key,value):
+        digits = 0
+        for i in value:
+            if i.isdigit():
+                digits+=1
+        if digits !=  10:
+            raise ValueError("Number must be 10 digits")
+        return value
+
+   
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,6 +48,30 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates("content")
+    def validate_content(self,key,value):
+        if len(value)<250:
+            raise ValueError("Content must be at least 250 characters")
+        return value
+
+    @validates("summary")
+    def validate_summary(self,key,value):
+        if len(value)>250:
+            raise ValueError("Summary must have a maximum of 250 characters")
+        return value
+
+    @validates("category")
+    def validate_category(self,key,value):
+        if value not in ["Non-Fiction","Fiction"]:
+            raise ValueError("Category must be Fiction or Non-Fiction")
+        return value
+    
+    @validates("title")
+    def validate_title(self,key,value):
+        if ("Won't Believe" in value) or ("Secret" in value) or ("Top" in value) or ("Guess" in value):
+            return value
+        else:
+            raise ValueError("Must have clickbait-y title")
 
 
     def __repr__(self):
